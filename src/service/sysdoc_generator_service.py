4a9e863b7383880ac86e589058f62a89db0518ea
NikitaVenediktov <n.venediktov@gmail.com>
2024-11-21 11:27:33 +0200

    refactor: update chat completion prompt and enhance system document schema

--- src/service/sysdoc_generator_service.py
+++ src/service/sysdoc_generator_service.py
@@ -32,13 +32,25 @@ client = AsyncOpenAI(
     api_key=OPENAI_API_KEY,
 )
 
+
 class SysDocSchema(BaseModel):
     """
     SysDocSchema is a Pydantic model that represents the schema for a system document.
-    Attributes:
-        system_doc (str): Сгенерированный текст системного документа. This field is required.
     """
-    system_doc: str = Field(..., description="Сгенерированный текст системного документа")
+    point_1: str = Field(..., description="Сгенерированный текст системного документа. Пункт 1. Введение (Цель документа, Список терминов и сокращений, Пользователи и заинтересованные стороны, Цели и задачи системы)")
+    point_2: str = Field(..., description="Сгенерированный текст системного документа. Пункт 2. Требования (Функциональные требования, Нефункциональные требования, Ограничения)")
+    point_3: str = Field(..., description="Сгенерированный текст системного документа. Пункт 3. Архитектурный дизайн (Общая архитектура, Диаграммы)")
+    point_4: str = Field(..., description="Сгенерированный текст системного документа. Пункт 4. Детализация компонентов (Модули и их взаимодействие, Интерфейсы)")
+    point_5: str = Field(..., description="Сгенерированный текст системного документа. Пункт 5. Дизайн данных (Модель данных, Хранилища данных)")
+    point_6: str = Field(..., description="Сгенерированный текст системного документа. Пункт 6. Пользовательский интерфейс (Описание UI или API EndPoint)")
+    point_7: str = Field(..., description="Сгенерированный текст системного документа. Пункт 7. Безопасность (Аутентификация и авторизация, Шифрование, Управление доступом)")
+    point_8: str = Field(..., description="Сгенерированный текст системного документа. Пункт 8. Производительность и масштабируемость (Требования к производительности, Стратегии масштабирования, Балансировка нагрузки)")
+    point_9: str = Field(..., description="Сгенерированный текст системного документа. Пункт 9. Интеграция с внешними системами (API и протоколы, Обработка ошибок и отказов)")
+    point_10: str = Field(..., description="Сгенерированный текст системного документа. Пункт 10. Развертывание и инфраструктура (Окружения, Инструменты и технологии, Процессы CI/CD)")
+    point_11: str = Field(..., description="Сгенерированный текст системного документа. Пункт 11. Тестирование (Стратегия тестирования, Инструменты тестирования, Критерии приемки)")
+    point_12: str = Field(..., description="Сгенерированный текст системного документа. Пункт 12. Мониторинг и логирование (Как система будет отслеживаться в реальном времени)")
+    point_13: str = Field(..., description="Сгенерированный текст системного документа. Пункт 13. Ограничения и предположения (Технические ограничения)")
+    point_14: str = Field(..., description="Сгенерированный текст системного документа. Пункт 14. Согласования и утверждения (История версий, Ответственные лица)")
 
 
 async def run_inference(system_message: str, user_message: str) -> Tuple[Optional[str], Optional[str]]:
@@ -67,20 +79,23 @@ async def run_inference(system_message: str, user_message: str) -> Tuple[Optiona
     except Exception as e:
         return None, str(e)
     
-def main(solution_document_path: str, system_design_document_path: str):
+def main(solution_document_path: str, system_design_document_path: str, researcher_comments_path: str):
     """
     Основная функция для запуска сервиса генерации системных документов.
     :param solution_document_path: Путь к файлу с документом решения
     :param system_design_document_path: Путь к файлу с документом системного дизайна
     """
-    # 1. Обработка PDF файла и извлечение текста
+    # 1. Обработка файла и извлечение текста
     parsed_text = "Красавчики, все верно!"
+    researcher_comments_path = parsed_text
 
+    solution_document = SOLUTION_DOCUMENT_EXAMPLE
+    solution_document_path = solution_document
     # 3. Запуск асинхронной функции run_inference
     system_message = CHAT_COMPLETION_PROMPT_SYSTEM
     user_message = CHAT_COMPLETION_PROMPT_USER.format(
-        solution_design_document=SOLUTION_DOCUMENT_EXAMPLE,
-        researcher_comments=parsed_text,
+        solution_design_document=solution_document_path,  # поменять тут
+        researcher_comments=researcher_comments_path,  # поменять тут
         sys_des_document_template=SYS_DOC_TEMPLATE
     )
     nest_asyncio.apply()
@@ -101,4 +116,8 @@ def main(solution_document_path: str, system_design_document_path: str):
 if __name__ == "__main__":
     solution_document_path = ""
     system_design_document_path = "data/final_sysdoc.json"
-    main(solution_document_path, system_design_document_path)
+    researcher_comments_path = ""
+    
+    main(solution_document_path,
+         system_design_document_path,
+         researcher_comments_path)
