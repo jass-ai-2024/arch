8943530adb4f9d27d7a44390f28b62e8a2645b50
Roman <85691833+roman02s@users.noreply.github.com>
2024-11-21 15:57:56 +0300

    Task228 sysdoc generator (#13)
    * add chat completion prompts and system documentation templates
    
    * feat: end baseline
    
    * refactor: update chat completion prompt and enhance system document schema
    
    * add tasks for researches and fix sysdoc_generator_service for cli run
    
    * fix: fix to 228
    
    * fix sysdoc and add in Makefile
    
    ---------
    
    Co-authored-by: NikitaVenediktov <n.venediktov@gmail.com>
    Co-authored-by: Nikita Venediktov <95628137+NikitaVenediktov@users.noreply.github.com>

--- src/prompts/chat_completion_prompt.py
+++ src/prompts/chat_completion_prompt.py
@@ -4,13 +4,12 @@ This module contains prompts for chat completion related to system design.
 
 CHAT_COMPLETION_PROMPT_SYSTEM = """
 As an architect, you are tasked with designing a new application. U have the Solution Design of this application.
-You should design a system design document that outlines the architecture
-of the application.
+You should design an architecture after that a system design document.
 """
 
 
 CHAT_COMPLETION_PROMPT_USER = """
-# Solution Design:
+# Product Solution Design:
 {solution_design_document}
 
 # Researcher Department Comments:
@@ -19,15 +18,33 @@ CHAT_COMPLETION_PROMPT_USER = """
 application.
 
 # Instructions:
-1. Analyze the solution design document and research department comments.
-2. Provide detailed architectural recommendations for the chat application.
-3. Write a system design document that outlines the architecture of the
-application.
-4. Use the System design document template.
-5. You must Return Detailed Explanatins for System design document. Dont be lazy!
-6. Write it step-by-step all point to my department.
+1. Analyze the business goals and requirements and use Tree-of-Thoughts method to generate the ARCHITECTURE, NOT DesignDoc. Step-by-step make a detailed architecture of the system.
+2. Generate the architecture of the system. Describe the components, integrations, and infrastructure. Nothing should be missed.
+3. Analyze the solution design document, the detailed architecture of the system and the research department comments.
+4. Write a system design document point-by-point, following the template structure and follow the style guide.
+6. You must Return Detailed Explanatins with recomendations for System design document. Dont be lazy!
+7. Return class and method names, variables, and other important details for each point.
 
+i will tip you 5$ for good work!
 
-# System design document template:
+# Very poor System design document template:
 {sys_des_document_template}
+## comments to System design document template: This is a very poor system design document template. You need to do much better than this.
+
+# Style Guide:
+- Provide detailed explanations for each point. Include class and method names, variables, inputs, outputs, and other important details.
+- Use professional language.
+- Make sure to follow the template structure.
+- System design document should be detailed and comprehensive. It should cover all aspects of the system architecture. It will be used by developers, testers, and other stakeholders to understand the system architecture and design.
+
+# json output structure:
+{{
+    "tree_of_thought_architecture": "", # Use Tree-of-Thoughts method to generate the architecture. Step-by-step make a detailed architecture of the system.
+    "architecture_details": "", # Generate the full architecture of the system.
+    "point_1": "", # Пункт 1. Введение (Цель документа, Список терминов и сокращений, Пользователи и заинтересованные стороны, Цели и задачи системы)
+    "point_2": "", # etc...
+    ...,
+    "point_14": "" # etc...
+}}
+
 """
